8bc4cc4b54d9075de35cc70137c6449a823d5707
EGOIST <0x142857@gmail.com>
2018-11-06 14:23:19 +0800

    tweaks

---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <VersionSelector slot="sidebar-top"/>
+  </Layout>
+</template>
+
+<script>
+import Layout from '@default-theme/Layout.vue'
+import VersionSelector from './VersionSelector.vue'
+export default {
+  components: {
+    Layout,
+    VersionSelector
+  }
+}
+</script>
---
+++ docs/.vuepress/theme/VersionSelector.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="version-selector">
+    <select @change="handleSelect">
+      <option value="v1" selected>v1</option>
+      <option value="v0">v0</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleSelect(e) {
+      if (e.target.value === 'v0') {
+        window.location.href = 'https://v0.saojs.org/'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.version-selector {
+  padding: 1.5rem 1.5rem 0;
+}
+</style>
